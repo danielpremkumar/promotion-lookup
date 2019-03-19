f9407d667d5678103a843daccfede311a51c4abb
Daniel RajKumar <DRajKumar@corp.local>
2019-03-19 17:52:21 +0530

    Swagger Metadata Added

--- src/main/java/com/xcc/promotion/lookup/controller/PromotionController.java
+++ src/main/java/com/xcc/promotion/lookup/controller/PromotionController.java
@@ -2,6 +2,9 @@ package com.xcc.promotion.lookup.controller;
 
 import com.xcc.promotion.lookup.manager.PromotionManager;
 import com.xcc.promotion.lookup.model.Promotion;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,14 @@ public class PromotionController {
         this.promotionManager = promotionManager;
     }
 
+    @ApiOperation(value = "List of all Active Promotion", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @RequestMapping(value = "/rest/promos", method = RequestMethod.GET)
     public @ResponseBody
     List<Promotion> getAllActivePromotion() {
--- src/main/java/com/xcc/promotion/lookup/swagger/config/SwaggerConfig.java
+++ src/main/java/com/xcc/promotion/lookup/swagger/config/SwaggerConfig.java
@@ -4,9 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +23,17 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.xcc.promotion.lookup"))
                 .paths(PathSelectors.regex("/rest/promos.*"))
-                .build();
+                .build()
+                .apiInfo(loadMetaData());
+    }
+
+    private ApiInfo loadMetaData() {
+        return new ApiInfo(
+                "Promotion Lookup Service",
+                "Service to Fetch all active promotion from XCC",
+                "1.0", "",
+                new Contact("Daniel Raj kumar", "", "danielpremkumar87@gmail.com"),
+                "", "", new ArrayList<VendorExtension>()
+        );
     }
 }
